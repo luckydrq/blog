c1271d4f990fa5156239602cfec133c13301a452
xuezu <ruoqi.drq@alibaba-inc.com>
2013-07-29 22:26:32 +0800

    add grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
+  	cssmin: {
+  	  options: {
+  	  	keepSpecialComments: false
+  	  },
+  	  combine: {
+  	  	files: {
+      	  './assets/css/lucky.css': ['./assets/css/blog.css', './assets/css/font.css', './assets/css/syntax.css']
+        }
+  	  }
+  	}
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin']);
+}
