d98bb359597085e8d43e9bf3ae15683c1f9d64eb
xuezu <ruoqi.drq@alibaba-inc.com>
2013-07-29 22:30:14 +0800

    format code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
   grunt.initConfig({
-  	pkg: grunt.file.readJSON('package.json'),
-  	cssmin: {
-  	  options: {
-  	  	keepSpecialComments: false
-  	  },
-  	  combine: {
-  	  	files: {
-      	  './assets/css/lucky.css': ['./assets/css/blog.css', './assets/css/font.css', './assets/css/syntax.css']
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      options: {
+        keepSpecialComments: false
+      },
+      combine: {
+        files: {
+          './assets/css/lucky.css': ['./assets/css/blog.css', './assets/css/font.css', './assets/css/syntax.css']
         }
-  	  }
-  	}
+      }
+    }
   })
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
